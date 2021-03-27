152a63c71d7aa86565cfcfd8d403ed4cae66782b
yatharthmago-stack <yatharthmago12@gmail.com>
2021-03-27 14:15:47 +0530

    Dashboard updated

--- app/src/main/java/com/acash/bechdo/MainActivity.kt
+++ app/src/main/java/com/acash/bechdo/MainActivity.kt
@@ -2,10 +2,31 @@ package com.acash.bechdo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val activityRef = this
+
+        val categories = Categories.getCategories()
+        val categoryAdapter = CategoryAdapter(categories, activityRef)
+
+        rvCategory.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true)
+        (rvCategory.layoutManager as LinearLayoutManager).scrollToPosition(categories.size-1)
+        rvCategory.adapter = categoryAdapter
+
+        val recents = Recents.getRecents()
+        val recentAdapter = RecentAdapter(recents)
+
+        rvRecent.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true)
+        (rvRecent.layoutManager as LinearLayoutManager).scrollToPosition(recents.size-1)
+        rvRecent.adapter = recentAdapter
     }
+
+
 }
