da7d46f781afb827cfa67e2cb0d5e276a6c2b389
Aakash Gupta <aakashguptaa23@gmail.com>
2021-04-23 03:52:25 +0530

    Added Navigation Drawer and post products feature (#20)

--- app/src/main/java/com/acash/bechdo/adapters/RecentAdapter.kt
+++ app/src/main/java/com/acash/bechdo/adapters/RecentAdapter.kt
@@ -5,11 +5,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.acash.bechdo.R
-import com.acash.bechdo.models.Recents
-import kotlinx.android.synthetic.main.category_row.view.image
+import com.acash.bechdo.models.Product
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recent_row.view.*
 
-class RecentAdapter(private val list: ArrayList<Recents>): RecyclerView.Adapter<RecentAdapter.RecentViewHolder>() {
+class RecentAdapter(private val list: ArrayList<Product>): RecyclerView.Adapter<RecentAdapter.RecentViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RecentViewHolder(
         LayoutInflater.from(parent.context).inflate(
@@ -18,12 +18,14 @@ class RecentAdapter(private val list: ArrayList<Recents>): RecyclerView.Adapter<
     )
 
     override fun onBindViewHolder(holder: RecentViewHolder, position: Int) {
-        val product: Recents = list[position]
+        val product: Product = list[position]
 
         holder.itemView.apply{
-            image.setImageResource(product.id)
-            tvRecentProduct.text = product.pname
-            tvRecentPrice.text = product.price
+            Picasso.get().load(product.downLoadUrlsPics[0]).into(image)
+            tvRecentProduct.isSelected = true
+            tvRecentPrice.isSelected = true
+            tvRecentProduct.text = product.title
+            tvRecentPrice.text = "₹ ${product.price}"
         }
     }
 
